1c5ddb5e6e38e3d0bdd3895fcaab4945226a8f0f
Peter Bondar <bondar.petro@lll.kpi.ua>
2022-09-26 21:44:12 +0300

    Addition + user representation

--- LongInt/LongInt.cpp
+++ LongInt/LongInt.cpp
@@ -1,7 +1,15 @@
 #include "LongInt.hpp"
+#include <exception>
+
+#define BASIC_ARRAY_SIZE 2
+#define MEMORY_BLOCK_SIZE 64
+#define MEMORY_BLOCK_SHIFT 63
+
+// static array used to convert integer value to hex_char
+char LongInt::hex_char[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
 // Default constructors
-LongInt::LongInt() : arr_size{64}
+LongInt::LongInt() : arr_size{BASIC_ARRAY_SIZE}
 {
     // Allocate memory and set all bits to 0
     data = new uint64_t[arr_size];
@@ -11,17 +19,16 @@ LongInt::LongInt() : arr_size{64}
     }
 }
 
-LongInt::LongInt(uint64_t _default_num) : arr_size{63}
+LongInt::LongInt(uint64_t _default_num) : arr_size{BASIC_ARRAY_SIZE}
 {
     // Allocate memory and set all bits to 0, except last value
     data = new uint64_t[arr_size + 1];
-    for(int i = 0; i < arr_size; ++i)
+    for(int i = 0; i < arr_size - 1; ++i)
     {
         data[i] = 0;
     }
     // Set last data to _default_num
-    ++arr_size;
-    data[arr_size + 1] = _default_num;
+    data[arr_size - 1] = _default_num;
 }
 
 // Copy constructor
@@ -46,4 +53,128 @@ LongInt::~LongInt()
 {
     if(data)
         delete[] data;
+}
+
+// Assignment operators
+LongInt& LongInt::operator=(const LongInt & cp)
+{
+    if(data)
+        delete[] data;
+
+    arr_size = cp.arr_size;
+    data = new uint64_t[arr_size];
+    for(int i = 0; i < arr_size; ++i)
+    {
+        data[i] = cp.data[i];
+    }
+
+    return *this;
+}
+
+LongInt& LongInt::operator=(LongInt && mv)
+{
+    if(data)
+        delete[] data;
+
+    arr_size = mv.arr_size;
+    data = mv.data;
+    mv.data = nullptr;
+
+    return *this;
+}
+
+LongInt& LongInt::operator=(unsigned long long _default_num)
+{
+    if(data)
+        delete[] data;
+    
+    arr_size = BASIC_ARRAY_SIZE;
+
+    // Allocate memory and set all bits to 0, except last value
+    data = new uint64_t[arr_size + 1];
+    for(int i = 0; i < arr_size - 1; ++i)
+    {
+        data[i] = 0;
+    }
+    // Set last data to _default_num
+    data[arr_size - 1] = _default_num;
+
+    return *this;
+}
+
+// Arithmetic operations
+LongInt LongInt::operator+(const LongInt& r) const
+{
+    // Temporary exception for different size (need further refactoring)
+    if(this->arr_size != r.arr_size)
+        throw std::out_of_range("Different size long addition called");
+
+    LongInt result;
+    // Carry for each step could be 0 or 1
+    uint64_t carry = 0;
+    for(int i = r.arr_size - 1; i >= 0; --i)
+    {
+        // Do standart addition on same memory block and check for carry
+        result.data[i] = this->data[i] + r.data[i] + carry;
+
+        // Carry-check boolean function
+        carry = (((this->data[i] & r.data[i]) | ((~result.data[i]) & (this->data[i] | r.data[i]))) >> MEMORY_BLOCK_SHIFT);
+    }
+
+    return result;
+}
+
+
+
+// User output
+std::string LongInt::to_hex() const
+{
+    // Create string
+    size_t parts = MEMORY_BLOCK_SIZE >> 2;
+    size_t len = parts * arr_size + 2;
+    std::string result(len, '0');
+
+    // Filling string from the end
+    size_t str_id = len - 1;
+
+    uint64_t mask = 15; // mask for last 4 bits
+    for(int arr_id = arr_size - 1; str_id > 1; --arr_id)
+    {
+        for(int block_id = 0; block_id < MEMORY_BLOCK_SIZE; block_id += 4)
+        {
+            // Check every 4 bits
+            int i = ((data[arr_id] >> block_id) & mask);
+            result[str_id--] = hex_char[(data[arr_id] >> block_id) & mask];
+        }
+    }
+
+    // Setting up prefix
+    result[str_id] = 'x';
+
+    return result;
+}
+
+
+
+std::string LongInt::to_binary() const
+{
+    // Create string
+    size_t len = (size_t)MEMORY_BLOCK_SIZE * arr_size + 2;
+    std::string result(len, '0');
+
+
+    // Filling string from the end
+    size_t str_id = len - 1;
+    for(int arr_id = arr_size - 1; str_id > 1; --arr_id)
+    {
+        for(int block_id = 0; block_id < MEMORY_BLOCK_SIZE; ++block_id, --str_id)
+        {
+            // Getting last bit after bit-shift
+            result[str_id] = (1 & (data[arr_id] >> block_id)) ? '1' : '0';
+        }
+    }
+
+    result[str_id] = 'b'; // setting prefix
+
+    return result;
 }
--- LongInt/LongInt.hpp
+++ LongInt/LongInt.hpp
@@ -1,7 +1,9 @@
 #include <cstdint>  // fixed size integer types
-#include <ostream>
 
-#define MEMORY_BLOCK_SIZE 64
+// Replace with predeclaration
+#include <ostream>
+#include <string>
+//
 
 /**
  * Integer class to handle 
@@ -9,8 +11,8 @@
  */
 class LongInt
 {
-    uint16_t arr_size;
     uint64_t* data = nullptr;
+    uint16_t arr_size;
 
 public:
     // Constructors and destructor
@@ -21,21 +23,21 @@ public:
     ~LongInt();
 
     // Basic binary operations
-    LongInt operator<<(uint16_t n) const;
-    LongInt operator>>(uint16_t n) const;
-    LongInt operator^(const LongInt& r) const;
-    LongInt operator&(const LongInt& r) const;
-    LongInt operator|(const LongInt& r) const;
-    LongInt operator~() const;
+    // LongInt operator<<(int n) const;
+    // LongInt operator>>(int n) const;
+    // LongInt operator^(const LongInt& r) const;
+    // LongInt operator&(const LongInt& r) const;
+    // LongInt operator|(const LongInt& r) const;
+    // LongInt operator~() const;
 
 
     // Basic arithmetic operations
     LongInt operator+(const LongInt& r) const;
-    LongInt operator-(const LongInt& r) const;
-    LongInt operator*(const LongInt& r) const;
-    LongInt operator/(const LongInt& r) const;
-    LongInt square() const;
-    LongInt power(const LongInt& r) const;
+    // LongInt operator-(const LongInt& r) const;
+    // LongInt operator*(const LongInt& r) const;
+    // LongInt operator/(const LongInt& r) const;
+    // LongInt square() const;
+    // LongInt power(const LongInt& r) const;
 
     // Assignment operators
     LongInt& operator=(const LongInt & cp);
@@ -43,13 +45,22 @@ public:
     LongInt& operator=(unsigned long long _default_num);
 
     // Compare operators
-    bool operator==(const LongInt& r) const;
-    bool operator<(const LongInt& r) const;
-    bool operator>(const LongInt& r) const;
-    bool operator<=(const LongInt& r) const;
-    bool operator>=(const LongInt& r) const;
-    operator bool() const;
+    // bool operator==(const LongInt& r) const;
+    // bool operator!=(const LongInt& r) const;
+    // bool operator<(const LongInt& r) const;
+    // bool operator>(const LongInt& r) const;
+    // bool operator<=(const LongInt& r) const;
+    // bool operator>=(const LongInt& r) const;
+    // operator bool() const;
 
     // User output
-    friend bool operator<<(std::ostream& out, const LongInt& num);
+    // friend bool operator<<(std::ostream& out, const LongInt& num);
+    std::string to_hex() const;
+    std::string to_binary() const;
+
+    // Utility functions
+    // void resize(uint16_t new_arr_size); 
+    // uint64_t* get_memory();
+private:
+    static char hex_char[];
 };
